88e263a3393f36dda4303f87c5b42ad145c11a8b
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-09-12 21:23:37 +0700

    chore: init

---
+++ src/main/java/com/example/spring_notes_explorer/SpringNotesExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_notes_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNotesExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNotesExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_notes_explorer/SpringNotesExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_notes_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNotesExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
